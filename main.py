f057d1b80daf16b810290691a5eba2537a931a1c
Sam <mckinney.samuel.b@gmail.com>
2019-09-03 21:32:30 +1000

    Made the program infinitely loop, added comments, no longer command line based.

--- main.py
+++ main.py
@@ -1,53 +1,71 @@
 from __future__ import print_function
 import yaml
-import sys
 
+# initialize the yml file to be read
 with open('recipes.yaml', 'r') as ymlfile:
     cfg = yaml.load(ymlfile)
 
+# initialize the variables
+infinite = True
 result = {}
 hex_result = {}
 color_list = []
 
-if len(sys.argv) >= 2:
-    filename = sys.argv[1]
-else:
-    print("You can search for the following colors:")
-    for x in cfg:
-        color_list.append(x)
-    i = 0
-    while i < len(color_list):
-        if i + 1 == len(cfg):
-            print(color_list[i])
-        else:
-            print(color_list[i], end=", ")
-        i += 1
-    exit()
+
+# commented out, I think I have a better way of doing this instead of using command line arguments.
+# # check the arguments, if there are any then continue, otherwise list available colours
+# if len(sys.argv) >= 2:
+#    dye_color_wanted = sys.argv[1]
+# else:
+#     print("You can search for the following colors:")
+#     for x in cfg:
+#         color_list.append(x)
+#     i = 0
+#     while i < len(color_list):
+#         if i + 1 == len(cfg):
+#             print(color_list[i])
+#         else:
+#             print(color_list[i], end=", ")
+#         i += 1
+#     exit()
+
+# Get user input for the colours wanted, ensuring the ability to quit if needed
+def which_color_do_you_want():
+    print("What colour dye are you looking for?")
+    return input().lower()
 
 
+# Find the color in the ymlfile
 def find_color(color):
     for section in cfg:
         if section == color:
             return cfg[section]
 
 
+# List the colors found, or error if none are found
 def color_result(color):
     if find_color(color):
         found = find_color(color)
+    elif color == "exit" or "quit" or "q":
+        print("\nThanks for coming by!")
+        exit()
     else:
-        print("This color doesn't exist, sorry.")
-        exit(0)
+        print("\nThis color doesn't exist, sorry.")
+        exit()
 
+    # if an item is found, check the ingredients that are either empty or 0 and remove them.
     if found.items():
         for x, y in found.items():
             if y == 0 or y == "":
                 continue
+            # ensure that the hexcode is a separate entry and isn't added with the ingredients
             if x == "hexcode":
                 hex_result[x] = y
                 continue
             result[x] = y
 
-    print("You need the following materials to make this color dye: \n")
+    # print the required materials as well as the hexcode
+    print("\n\nYou need the following materials to make " + color + " dye: \n")
     for x, y in result.items():
         print(x + ": " + str(y))
     print("\nThis color has the following hexcode: \n")
@@ -55,4 +73,7 @@ def color_result(color):
         print(x + ": " + y)
 
 
-color_result(filename)
+# infinitely loop through this program until exited.
+while infinite:
+    coloring = which_color_do_you_want()
+    color_result(coloring)
