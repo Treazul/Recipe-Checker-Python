7062f8a5676f5ea8e65f7932723a3f73a6f658e6
Sam <mckinney.samuel.b@gmail.com>
2019-09-05 11:55:51 +1000

    Lowered the window size. Moved the header around and gave it some styles. Moved widgets around to try and fit better on new window size Created some default variables for positioning to allow for editing ease. Increased spacing of items. Updated stylesheet to be more in style with standard use.

--- gui.py
+++ gui.py
@@ -11,8 +11,8 @@ class App(QWidget):
         self.title = 'Ark Dye Checker'
         self.left = 10
         self.top = 10
-        self.width = 640
-        self.height = 480
+        self.width = 480
+        self.height = 400
         self.initUI()
 
     # TODO: Pretty up the UI generally
@@ -28,23 +28,31 @@ class App(QWidget):
         ingredients = color_result[0]
         hex_code = color_result[1]
         i = 0
+        default_y_value = 70
+        default_x_value = 110
+        default_x_value_2 = default_x_value * 2.5
+        header_y_value = 30
+        button_y_value = default_y_value + 60
+        header_x_value = 12
+
         if len(color_result) == 2:
-            header = QLabel("To make " + choice + " you use the following ingredients", self)
-            header.move(30, 30)
+            header = QLabel("To make " + choice + " dye you use the following ingredients", self)
+            header.move(header_x_value, header_y_value)
+            header.setStyleSheet("QLabel { font: bold 18px; border-bottom-width: 2px; border-bottom-color: black; "
+                                 "border-style: outset; border-radius: 10px; }")
             for x, y in ingredients.items():
                 ingredient = QLabel(x, self)
                 amount = QLabel(str(y), self)
-                ingredient.move(50, 50 + i)
-                ingredient.setStyleSheet("QLabel { font-weight: Bold;}")
-                amount.move(160, 50 + i)
-                i += 20
+                ingredient.move(default_x_value, default_y_value + i)
+                ingredient.setStyleSheet("QLabel { font: bold;}")
+                amount.move(default_x_value_2, default_y_value + i)
+                i += 22
             for x, y in hex_code.items():
                 label = QLabel(x, self)
                 code = QLabel(y, self)
-                label.move(50, 50 + i)
-                label.setStyleSheet("QLabel { font-weight: Bold;}")
-
-                code.move(160, 50 + i)
+                label.move(default_x_value, default_y_value + i)
+                label.setStyleSheet("QLabel { font: bold;}")
+                code.move(default_x_value_2, default_y_value + i)
         else:
             label_warning = QLabel("Something went wrong! Please tell the creator as soon as possible. No error code "
                                    "has been logged", self)
@@ -53,11 +61,11 @@ class App(QWidget):
         # Load a button to go back
         choose_again = QPushButton('Choose Again', self)
         choose_again.setToolTip('Go back to the choices')
-        choose_again.move(50, 100 + i)
+        choose_again.move(default_x_value, button_y_value + i)
         choose_again.clicked.connect(self.on_click)
         exit_button = QPushButton('Close', self)
         exit_button.setToolTip('Close the program')
-        exit_button.move(200, 100 + i)
+        exit_button.move(default_x_value * 2.2, button_y_value + i)
         exit_button.clicked.connect(self.exit_click)
 
         # Show the Gui
